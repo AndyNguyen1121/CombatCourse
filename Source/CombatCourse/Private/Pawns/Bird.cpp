97d5eb356738267f734070117ac472eae3c8c669
AndyNguyen1121 <andyhoangnguyen2005@gmail.com>
2025-09-21 01:28:23 -0700

    add crow movement and camera

--- Source/CombatCourse/Private/Pawns/Bird.cpp
+++ Source/CombatCourse/Private/Pawns/Bird.cpp
@@ -2,18 +2,72 @@
 
 
 #include "Pawns/Bird.h"
+#include "Components/CapsuleComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 ABird::ABird()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Collider
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(FName("Capsule"));
+	SetRootComponent(Capsule);
+
+	// Mesh
+	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>("BirdMesh");
+	BirdMesh->SetupAttachment(Capsule);
+
+	// Spring Arm
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	SpringArm->SetupAttachment(Capsule);
+	SpringArm->TargetArmLength = 300.f;
+
+	// Camera
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(SpringArm);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 void ABird::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(BirdMappingContext, 0);
+		}
+	}
+}
+
+void ABird::Move(const FInputActionValue& Value)
+{
+	FVector2D MovementInput = Value.Get<FVector2D>();
+
+	if (!MovementInput.IsNearlyZero())
+	{
+		MovementInput.Normalize();
+		AddMovementInput(GetActorForwardVector(), MovementInput.Y);
+		AddMovementInput(GetActorRightVector(), MovementInput.X);
+	}
+	
+	GEngine->AddOnScreenDebugMessage(0, 3, FColor::Red, FString::Printf(TEXT("%s"), *MovementInput.ToString()));
+}
+
+void ABird::Turn(const FInputActionValue& Value)
+{
+	FVector2D RotationInput = Value.Get<FVector2D>();
+	
+	RotationInput.Y = FMath::Clamp(GetActorRotation().Pitch + RotationInput.Y, -75.f, 75.f);
+	Controller->SetControlRotation(FRotator(RotationInput.Y, GetActorRotation().Yaw + RotationInput.X, 0));
+	
+	GEngine->AddOnScreenDebugMessage(1, 3, FColor::Red, FString::Printf(TEXT("%s"), *RotationInput.ToString()));
 }
 
 void ABird::Tick(float DeltaTime)
@@ -25,6 +79,12 @@ void ABird::Tick(float DeltaTime)
 void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABird::Move);
+		EnhancedInputComponent->BindAction(TurnAction, ETriggerEvent::Triggered, this, &ABird::Turn);
+	}
 
 }
 
--- Source/CombatCourse/Public/Pawns/Bird.h
+++ Source/CombatCourse/Public/Pawns/Bird.h
@@ -1,10 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
+#include "EnhancedInputComponent.h"
 #include "Bird.generated.h"
+
+class UCapsuleComponent;
+class UInputMappingContext;
+class UInputAction;
+class USpringArmComponent;
+class UCameraComponent;	
 
 UCLASS()
 class COMBATCOURSE_API ABird : public APawn
@@ -16,6 +23,33 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	
+
 protected:
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* BirdMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* TurnAction;
+	
+	void Move(const FInputActionValue& Value);
+	void Turn(const FInputActionValue& Value);
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UCapsuleComponent* Capsule;
+
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* BirdMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* Camera;
+	
 };
